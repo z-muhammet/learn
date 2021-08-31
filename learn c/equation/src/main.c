8d04951df8060c5d767cc4a3c9c92040aee2aa73
z-muhammet <88462420+z-muhammet@users.noreply.github.com>
2021-08-31 23:27:09 +0300

    infinite loop

--- learn c/equation/src/main.c
+++ learn c/equation/src/main.c
@@ -3,39 +3,47 @@
 
 int main()
 {
-	int sel1;
-	float s1,s2,s3,s4,s5;
-	printf("f(x):ax^2+bx+c (1)\ng(x):ax^3+bx^2+cx+d (2)\npls u select: ");
-	scanf("%d",&sel1);
-	switch (sel1)
+	int sel1 = 0, i;
+
+	float s1, s2, s3, s4, s5;
+	while (sel1 != 3)
 	{
-	case 1: 
-	printf("pls u enter 'a' :");
-	scanf("%f",&s1);
-	printf("pls u enter 'b' :");
-	scanf("%f",&s2);
-	printf("pls u enter 'c' :");
-	scanf("%f",&s4);
-	printf("pls u enter 'x' :");
-	scanf("%f",&s3);
-	printf("u need this: %.3f",s1*s3*s3+s2*s3+s4);
+		printf("f(x):ax^2+bx+c (1)\ng(x):ax^3+bx^2+cx+d (2)\nexit (3)\npls u select: ");
+		scanf("%d", &sel1);
+		switch (sel1)
+		{
+		case 1:
+			printf("pls u enter 'a' :");
+			scanf("%f", &s1);
+			printf("pls u enter 'b' :");
+			scanf("%f", &s2);
+			printf("pls u enter 'c' :");
+			scanf("%f", &s4);
+			printf("pls u enter 'x' :");
+			scanf("%f", &s3);
+			printf("u need this: %.3f\n", s1 * s3 * s3 + s2 * s3 + s4);
+			break;
+		case 2:
+			printf("pls u enter 'a' :");
+			scanf("%f", &s1);
+			printf("pls u enter 'b' :");
+			scanf("%f", &s2);
+			printf("pls u enter 'c' :");
+			scanf("%f", &s4);
+			printf("pls u enter 'd' :");
+			scanf("%f", &s5);
+			printf("pls u enter 'x' :");
+			scanf("%f", &s3);
+			printf("u need this: %.3f\n", (float)s1 * s3 * s3 * s3 + s2 * s3 * s3 + s4 * s3 + s5);
+			break;
+		case 3:
 		break;
-	case 2:
-	printf("pls u enter 'a' :");
-	scanf("%f",&s1);
-	printf("pls u enter 'b' :");
-	scanf("%f",&s2);
-	printf("pls u enter 'c' :");
-	scanf("%f",&s4);
-	printf("pls u enter 'd' :");
-	scanf("%f",&s5);
-	printf("pls u enter 'x' :");
-	scanf("%f",&s3);
-	printf("u need this: %.3f",(float)s1*s3*s3*s3+s2*s3*s3+s4*s3+s5);
-	break;
-	default:printf("pls try again");
-		break;
+		default:
+			printf("pls try again");
+			break;
+		}
 	}
+
 	system("pause");
 	return (0);
 }
