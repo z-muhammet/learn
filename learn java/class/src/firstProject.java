b8783bfe3436e7aa1c78620667f3cc70f7821560
muhammet zeren <88462420+z-muhammet@users.noreply.github.com>
2022-02-13 21:17:27 +0300

    i learn classes

--- learn java/class/src/firstProject.java
+++ learn java/class/src/firstProject.java
@@ -5,37 +5,44 @@ import java.util.Scanner;
 public class firstProject {
     public static void main(String[] args) throws IOException {
         Scanner scan = new Scanner(System.in);
-        String Personalname, Personalsurname, Personalposition, PersonalId;
+        String pid, Personalname, Personalsurname, Personalposition, PersonalId;
         double wage;
-        int Sel = 0;//timer=0;
+        int Sel = 0;// timer=0;
         File hebele = new File("hebele.txt");
         if (!hebele.exists()) {
             hebele.createNewFile();
         }
-        while (Sel != 3) {
-            System.out.println("Pls u Select (add new personal press '1', for list personalss '2', for exit '3')");
-           Sel= scan.nextInt();
-            if (Sel <= 3 && Sel > 0) {
+        while (Sel != 4) {
+            System.out.print(
+                    "Pls u Select (add new personal press '1', for list personals '2',for edit personal '3', for exit '4'): ");
+            Sel = scan.nextInt();
+            if (Sel <= 4 && Sel > 0) {
                 switch (Sel) {
                     case 1:
-                    System.out.println("pls u enter Personal Name: ");
-                    Personalname =  scan.next();
-                    System.out.println("pls u enter Personal ID: ");    
-                    PersonalId = scan.next();
-                    System.out.println("pls u enter Personal Surname: ");
-                    Personalsurname =  scan.next();
-                    System.out.println("pls u enter Personal Positiion: ");
-                    Personalposition =  scan.next();
-                    System.out.println("pls u enter Personal wage: ");
-                    wage =  scan.nextDouble();
-                    menage.add(hebele, PersonalId, Personalname, Personalsurname, Personalposition, wage);
-                   // timer++;
+                        System.out.println("pls u enter Personal ID: ");
+                        PersonalId = scan.next();
+                        System.out.println("pls u enter Personal Name: ");
+                        Personalname = scan.next();
+                        System.out.println("pls u enter Personal Surname: ");
+                        Personalsurname = scan.next();
+                        System.out.println("pls u enter Personal Positiion: ");
+                        Personalposition = scan.next();
+                        System.out.println("pls u enter Personal wage: ");
+                        wage = scan.nextDouble();
+                        menage.add(hebele, PersonalId, Personalname, Personalsurname, Personalposition, wage);
+                        // timer++;
                         break;
-                    case 2:
-                        menage.list(hebele,10);
+                    case 3:
+                        menage.list(hebele, 10);
+                        System.out.print("pls u enter personal id: ");
+                        pid = scan.next();
+                        menage.edit(hebele, 10, pid);
                         break;
 
-                    case 3:
+                    case 2:
+                        menage.list(hebele, 5);
+                        break;
+                    case 4:
                         System.out.println("Good lucky");
                         break;
                     default:
@@ -44,7 +51,6 @@ public class firstProject {
             }
         }
 
-        
         scan.close();
     }
 }
--- learn java/class/src/menage.java
+++ learn java/class/src/menage.java
@@ -15,10 +15,11 @@ public class menage {
         printWriter.close();
     }
 
-    public static void list(File fname, int timer) throws IOException {
-        String cont2[] = new String[timer], cont, Pid[] = new String[25], Pname[] = new String[25],Psurname[] = new String[25], Pposition[] = new String[25];
+    public static void edit(File fname, int timer, String personid) throws IOException {
+        String cont2[] = new String[timer], cont, Pid[] = new String[25], Pname[] = new String[25],
+                Psurname[] = new String[25], Pposition[] = new String[25];
         double Pwage[] = new double[25];
-        int i = 0;
+        int i = 0, hubele=0;
         Scanner fileScanner = new Scanner(fname);
         for (; i < timer; i++) {
             cont = fileScanner.nextLine();
@@ -26,26 +27,64 @@ public class menage {
             for (int j = 1; j <= 9; j += 2) {
                 switch (j) {
                     case 1:
-                        Pid[i]=cont2[j];
+                        Pid[i] = cont2[j];
                         break;
                     case 3:
-                        Pname[i]=cont2[j];
+                        Pname[i] = cont2[j];
                         break;
                     case 5:
-                        Psurname[i]=cont2[j];
+                        Psurname[i] = cont2[j];
                         break;
                     case 7:
-                        Pposition[i]=cont2[j];
+                        Pposition[i] = cont2[j];
                         break;
                     case 9:
-                        
-                        Pwage[i]= Double.parseDouble(cont2[j].replace(',', '.'));
+
+                        Pwage[i] = Double.parseDouble(cont2[j].replace(',', '.'));
                         break;
                     default:
                         break;
                 }
             }
         }
+        for (int k = 0; k < timer; k++) {
+            if (Pid[k] == personid) {
+                hubele = k;
+                break;
+            }
+        }
+        Scanner scanf = new Scanner(System.in);
+        System.out.println("pls u enter Personal ID: ");
+        Pid[hubele] = scanf.next();
+        System.out.println("pls u enter Personal Name: ");
+        Pname[hubele] = scanf.next();
+        System.out.println("pls u enter Personal Surname: ");
+        Psurname[hubele] = scanf.next();
+        System.out.println("pls u enter Personal Positiion: ");
+        Pposition[hubele] = scanf.next();
+        System.out.println("pls u enter Personal wage: ");
+        Pwage[hubele] = scanf.nextDouble();
+        FileWriter fileWriter = new FileWriter(fname, false);
+        PrintWriter printWriter = new PrintWriter(fileWriter);
+        for (int j = 0; j < Pid.length; j++) {
+            printWriter.printf("id: %s name: %s surname: %s position: %s wage: %.3f \n", Pid[j], Pname[j], Psurname[j],
+                Pposition[j], Pwage[j]);
+        }
+
+        printWriter.close();
+        scanf.close();
         fileScanner.close();
+    }
+
+    public static void list(File fname, int timer) throws IOException {
+        Scanner scan = new Scanner(fname);
+        String[] list = new String[timer];
+        for (int i = 0; i < timer; i++) {
+            list[i] = scan.nextLine();
+            System.out.println(list[i]);
+        }
+
+        scan.close();
+
     }
 }
