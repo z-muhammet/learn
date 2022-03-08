54067560906e8a188454954cecc9dfe547503044
muhammet zeren <88462420+z-muhammet@users.noreply.github.com>
2022-03-08 20:51:40 +0300

    Update App.java

--- learn java/dataStructures/linkedList/src/App.java
+++ learn java/dataStructures/linkedList/src/App.java
@@ -4,7 +4,7 @@ public class App {
     public static void main(String[] args) throws IOException {
         int select = 0;
         while (select != 5) {
-            System.out.printf("");
+            System.out.printf("pls u select (add prepend (1), add last (2),  ");
 
         }
     }
