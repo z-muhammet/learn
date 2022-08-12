2759f40f459ef5ab0898013c5b3134401061197b
muhammet zeren <88462420+z-muhammet@users.noreply.github.com>
2022-08-12 03:10:07 +0300

    i try
    layout in code

--- learnKotlin/MyFirstProject/app/src/main/java/com/example/myfirstproject/MainActivity.kt
+++ learnKotlin/MyFirstProject/app/src/main/java/com/example/myfirstproject/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.myfirstproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun screen(view: View) {
+        finish()
+        val  intent = Intent(this,MainActivity3::class.java)
+        val heb = null
+        Log.w(heb, "screen: ", )
+        startActivity(intent)
+    }
+
 }
