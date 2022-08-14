0e668fd8f75398f84675e17207176e28fc4759ef
muhammet zeren <88462420+z-muhammet@users.noreply.github.com>
2022-08-14 15:56:14 +0300

    drawable ex.

--- learnKotlin/MyFirstProject/app/src/main/java/com/example/myfirstproject/MainActivity3.kt
+++ learnKotlin/MyFirstProject/app/src/main/java/com/example/myfirstproject/MainActivity3.kt
@@ -1,11 +1,18 @@
 package com.example.myfirstproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main3)
+    }
+
+    fun runActivity (view: View){
+      val intent = Intent(this,multiLang::class.java)
+        startActivity(intent)
     }
 }
