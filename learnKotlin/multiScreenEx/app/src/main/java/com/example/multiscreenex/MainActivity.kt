b64416ca53b362072be582f8ed7255bc1b66c981
muhammet zeren <88462420+z-muhammet@users.noreply.github.com>
2022-08-20 01:59:51 +0300

    okay this work ha

--- learnKotlin/multiScreenEx/app/src/main/java/com/example/multiscreenex/MainActivity.kt
+++ learnKotlin/multiScreenEx/app/src/main/java/com/example/multiscreenex/MainActivity.kt
@@ -1,19 +1,42 @@
 package com.example.multiscreenex
 
+
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    findViewById<Button>(R.id.goactivityTwo).setOnClickListener {
-        startActivityForResult(Intent(this@MainActivity,activity2::class.java),0)
-    }
-    }
+        var hebele = findViewById<TextView>(R.id.mainData)
+        fun alertDia(Title: String, Message: String) {
+            val alert = AlertDialog.Builder(this)
+            alert.setTitle(Title).setMessage(Message)
+            alert.setPositiveButton("ok", DialogInterface.OnClickListener { dialogInterface, i ->
+                hebele.text = "asdasd"
+            })
+            alert.create().show()
+        }
 
+        val heb = Intent(applicationContext, activity2::class.java)
+        val intent = intent
+        val dataFrom = intent.getStringExtra("dataActTwo")
+
+        if (dataFrom == "") alertDia("Data", "u haven't a Data") else hebele.text = dataFrom
+
+
+        findViewById<Button>(R.id.goactivityTwo).setOnClickListener {
+            val editTextData = findViewById<EditText>(R.id.transferData).text.toString()
+            heb.putExtra("mainActData", editTextData)
+            startActivity(heb)
+            finish()
+        }
+    }
 
 }
--- learnKotlin/multiScreenEx/app/src/main/java/com/example/multiscreenex/activity2.kt
+++ learnKotlin/multiScreenEx/app/src/main/java/com/example/multiscreenex/activity2.kt
@@ -1,11 +1,37 @@
 package com.example.multiscreenex
 
+import android.app.AlertDialog
+import android.content.DialogInterface
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class activity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity2)
+        var hebele = findViewById<TextView>(R.id.actTwoData)
+        fun alertDia(Title: String, Message: String) {
+            val alert = AlertDialog.Builder(this)
+            alert.setTitle(Title)
+            alert.setMessage(Message)
+            alert.setPositiveButton("ok", DialogInterface.OnClickListener { dialogInterface, i ->
+                   hebele.text = null
+            })
+            alert.create().show()
+        }
+        val intentData = Intent(applicationContext,MainActivity::class.java)
+        val intent = intent
+        val data = intent.getStringExtra("mainActData")
+        if (data == "") alertDia("Data", "u haven't a Data") else  { hebele.text = data }
+        findViewById<Button>(R.id.goBack).setOnClickListener {
+            val dataBack =findViewById<EditText>(R.id.backData).text.toString()
+            intentData.putExtra("dataActTwo",dataBack)
+            startActivity(intentData)
+            finish()
+        }
     }
 }
